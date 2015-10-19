f90660a23da04ad7b7d2c00ba4b99bd178a3f8e7
Botmasher <languagejosh@hotmail.com>
2015-10-18 20:39:43 -0400

    Setup input scripts to test Shimmid animation controller and horizontal axis movement

---
+++ InputManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+	
+	public GameObject player;
+	[Range(0,500)]public float playerSpeed = 100f;
+
+	float horiz;
+	float vert;
+	bool action;
+
+
+	void Start () {
+	}
+
+
+	void Update () {
+
+		// check for button inputs
+		horiz = Input.GetAxis ("Horizontal");
+		vert = Input.GetAxis ("Vertical");
+		action = Input.GetButtonDown ("Jump");
+
+		if (action) {
+			if (player.GetComponent<Animator> ().GetBool ("isShell")) {
+				player.GetComponent<Animator> ().SetBool ("isShell", false);
+			} else {
+				player.GetComponent<Animator> ().SetBool ("isShell", true);
+			}
+		} else if (Mathf.Abs (horiz) > 0f) {
+			player.GetComponent<Animator> ().SetBool ("isWalking", true);
+			player.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (horiz * playerSpeed * Time.deltaTime, 0f));
+		} else {
+			player.GetComponent<Animator> ().SetBool ("isWalking", false);
+		}
+
+		if (vert != 0f) {}
+
+	}
+
+}
